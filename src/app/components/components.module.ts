ff101512fdbeafbadab60a28862bf3487ede2992
Antonio <antonio_ruescas@hotmail.com>
2022-03-03 01:18:35 +0100

    login funcionando

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagesComponent } from './images/images.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    ImagesComponent
+    ImagesComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     NgbCarouselModule, //Empaquetado del carrousel
-    FormsModule //Controles del formulario
+    FormsModule,
+    ReactiveFormsModule
+     //Controles del formulario
   ]
 })
 export class ComponentsModule { }
